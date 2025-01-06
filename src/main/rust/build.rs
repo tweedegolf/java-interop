bcd629302d790146155971ebf43f69ac12ef427f
Michiel <michielp1807@gmail.com>
2025-01-06 11:02:10 +0100

    Add jextract to build.rs

--- src/main/rust/build.rs
+++ src/main/rust/build.rs
@@ -1,16 +1,26 @@
-use std::env;
-
 fn main() {
-    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-
-    std::fs::remove_dir_all("./bindings").ok();
-    std::fs::create_dir_all("./bindings").unwrap();
-
-    // Invoke cbindgen
+    // Create C headers with cbindgen
+    let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     cbindgen::Builder::new()
-        .with_crate(crate_dir)
+        .with_crate(crate_dir.clone())
         .with_language(cbindgen::Language::C)
         .generate()
         .unwrap()
         .write_to_file("bindings/java_interop.h");
+
+    // Create Java interface with JExtract
+    let java_project_dir = std::path::Path::new(&crate_dir).ancestors().nth(3).unwrap();
+    std::process::Command::new("jextract")
+        .current_dir(java_project_dir)
+        .arg("--include-dir")
+        .arg("src/main/rust/bindings/")
+        .arg("--output")
+        .arg("src/main/java")
+        .arg("--target-package")
+        .arg("golf.tweede.gen")
+        .arg("--library")
+        .arg("java_interop")
+        .arg("src/main/rust/bindings/java_interop.h")
+        .spawn()
+        .unwrap();
 }
